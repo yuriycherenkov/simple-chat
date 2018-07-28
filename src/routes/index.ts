7d8ac25bad982b51f86f75a51f603357bbd56c73
yuriycherenkov <cherenkov.yuriy111@gmail.com>
2018-07-28 20:02:25 +0300

    test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import ChatHistory from './ChatHistory';
 
+const test = 'test';
 export const Routes = {
   Index: '/',
 };
