1f6a82a85334d3289e5455bad40d7d6752806b54
yuriycherenkov <cherenkov.yuriy111@gmail.com>
2018-07-28 19:57:36 +0300

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import * as React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './store';
+import Layout from './routes/Layout';
 
-import Routes from './routes';
+import Router from './routes';
 import './main.scss';
 
 const App = () => (
   <Provider store={store}>
     <BrowserRouter>
-      <Routes />
+    <Layout>
+      <Router />
+    </Layout>
     </BrowserRouter>
   </Provider>
 );
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,15 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import ChatHistory from './ChatHistory';
-import Layout from './Layout';
 
-// interface IRoute extends Switch<React.Props<any> {
+export const Routes = {
+  Index: '/',
+};
 
-// }
-
-const Routes: React.SFC<IRoute> = () => (
-  <Layout>
-    <Switch>
-      <Route exact path="/" component={ChatHistory} />
-    </Switch>
-  </Layout>
+const Router = () => (
+  <Switch>
+    <Route exact path={Routes.Index} component={ChatHistory} />
+  </Switch>
 );
 
-export default Routes;
+export default Router;
